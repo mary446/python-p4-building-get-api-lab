d51d53e7bb3dda128ba7c23dc8c9bae329cf253e
mary446 <135317707+mary446@users.noreply.github.com>
2023-09-21 07:33:20 +0300

    initial commit

--- server/app.py
+++ server/app.py
@@ -4,7 +4,7 @@ from flask import Flask, make_response, jsonify
 from flask_migrate import Migrate
 
 from models import db, Bakery, BakedGood
-
+import json
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -20,19 +20,60 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+
+    bakeries = []
+    for bakery in Bakery.query.all():
+        bakery_dict = bakery.to_dict()
+        bakeries.append(bakery_dict)
+
+    response = make_response(jsonify(bakeries), 200)
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter(Bakery.id == id).first()
+
+    bakery_dict = bakery.to_dict()
+    json_str = json.dumps(bakery_dict, indent=4)
+
+    response = make_response(json_str, 200)
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_list = []
+
+    baked_goods = BakedGood.query.order_by(BakedGood.price).all()
+
+    for baked in baked_goods:
+        baked_dic = baked.to_dict()
+        baked_list.append(baked_dic)
+        
+    json_str = json.dumps(baked_list, indent=4)
+
+    response = make_response(json_str, 200)
+
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).limit(1).first()
+
+    baked_dict = baked_goods.to_dict()
+
+    json_str = json.dumps(baked_dict, indent=4)
+
+    response = make_response(json_str, 200)
+
+    response.headers["Content-Type"] = "application/json"
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
